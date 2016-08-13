77e070c819113a67ff36fa182f1d562a53296894
Thiago Retondar <t.retondar@gmail.com>
2016-08-12 23:54:49 -0300

    studying another way to get the info about foreground proccess
    (not working yet)

--- app/src/main/java/fei/tcc/parentalcontrol/service/AppUsageInfoService.java
+++ app/src/main/java/fei/tcc/parentalcontrol/service/AppUsageInfoService.java
@@ -1,9 +1,16 @@
 package fei.tcc.parentalcontrol.service;
 
+import android.app.ActivityManager;
 import android.app.IntentService;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStatsManager;
 import android.content.Intent;
 import android.os.Handler;
 import android.util.Log;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
 
 import fei.tcc.parentalcontrol.component.ProcessManager;
 import fei.tcc.parentalcontrol.dao.PackageDao;
@@ -11,6 +18,8 @@ import fei.tcc.parentalcontrol.dao.PackageDao;
 public class AppUsageInfoService extends IntentService {
 
     private static final String TAG = "MyService";
+
+    private UsageStatsManager usageStatsManager;
 
     public AppUsageInfoService(String name) {
         super(name);
@@ -22,6 +31,7 @@ public class AppUsageInfoService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        usageStatsManager = ((UsageStatsManager) this.getSystemService("usagestats"));
         Log.i(TAG, "Antes do RUN");
     }
 
@@ -32,21 +42,62 @@ public class AppUsageInfoService extends IntentService {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                AppUsageInfoService.this.getForegroundApp();
-                handler.postDelayed(this, 1000);
+                //AppUsageInfoService.this.getForegroundApp();
+//                Log.i(TAG, "FOREGROUND APP: " + getApplicationInForeground());
+
+                List<ActivityManager.RunningAppProcessInfo> procInfos=getRunningAps();
+                int loop=0;
+                for (ActivityManager.RunningAppProcessInfo runningAppProcessInfo : procInfos) {
+                    //System.out.println("Process Name=====>"+procInfos.get(loop).processName);
+                    Log.i(TAG, "Process Name=====>"+procInfos.get(loop).processName);
+                    if(procInfos.get(loop).importance== ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+                    }
+                    ++loop;
+                }
+                handler.postDelayed(this, 3000);
             }
         }, 2000);
 
         return super.onStartCommand(intent, flags, startId);
+
+       // return super.onStartCommand(intent, flags, startId);
     }
 
     public void getForegroundApp() {
         PackageDao packageDao = new PackageDao(this);
 
-        packageDao.insert(ProcessManager.getForegroundApp());
+        String foregroundApp = ProcessManager.getForegroundApp();
+
+        packageDao.insert(foregroundApp);
+
+        Log.i(TAG, "FOREGROUND APP: " + foregroundApp);
 
         packageDao.close();
 
-//        Log.i(TAG, "FOREGROUND APP: " + ProcessManager.getForegroundApp());
     }
+
+    public String getApplicationInForeground() {
+        long time = System.currentTimeMillis();
+        UsageEvents usageEvent = usageStatsManager.queryEvents(time - 100 * 1000, time);
+        UsageEvents.Event event = new UsageEvents.Event();
+
+        while (usageEvent.hasNextEvent()) {
+            usageEvent.getNextEvent(event);
+        }
+        if (event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+            return event.getPackageName();
+        }
+        return "ANY PACKAGE FOUND";
+    }
+
+    /**
+     * This method returns list of running application.
+     * @return List<RunningAppProcessInfo>: containing application informations.
+     */
+    private List<ActivityManager.RunningAppProcessInfo> getRunningAps(){
+        ActivityManager actvityManager = (ActivityManager)this.getSystemService( ACTIVITY_SERVICE );
+        List<ActivityManager.RunningAppProcessInfo> procInfos = actvityManager.getRunningAppProcesses();
+        return procInfos;
+    }
+
 }
