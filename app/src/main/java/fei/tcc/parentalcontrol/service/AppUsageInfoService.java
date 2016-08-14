bf4789808f1061c1827a9ed3d163beae470a018d
Thiago Retondar <t.retondar@gmail.com>
2016-08-14 19:01:41 -0300

    refactoring and only getting information when phone is unlocked

--- app/src/main/java/fei/tcc/parentalcontrol/service/AppUsageInfoService.java
+++ app/src/main/java/fei/tcc/parentalcontrol/service/AppUsageInfoService.java
@@ -1,17 +1,21 @@
 package fei.tcc.parentalcontrol.service;
 
 import android.app.IntentService;
+import android.app.KeyguardManager;
 import android.app.usage.UsageStatsManager;
 import android.content.Intent;
 import android.os.Handler;
 import android.util.Log;
 
-import fei.tcc.parentalcontrol.component.ProcessManager;
-import fei.tcc.parentalcontrol.dao.PackageDao;
+import static fei.tcc.parentalcontrol.component.ProcessManager.getForegroundApp;
 
 public class AppUsageInfoService extends IntentService {
 
-    private static final String TAG = "MyService";
+    private static final String TAG = "AppUsageInfoService";
+
+    private static final Integer DELAY_SCREEN_UNLOCKED = 2000; // 2 seconds in ms
+
+    private static final Integer DELAY_SCREEN_LOCKED = 15000; // 15 seconds in ms
 
     private UsageStatsManager usageStatsManager;
 
@@ -26,7 +30,6 @@ public class AppUsageInfoService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         usageStatsManager = ((UsageStatsManager) this.getSystemService(USAGE_STATS_SERVICE));
-        Log.i(TAG, "Antes do RUN");
     }
 
     @Override
@@ -36,26 +39,31 @@ public class AppUsageInfoService extends IntentService {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                AppUsageInfoService.this.getForegroundApp();
-                handler.postDelayed(this, 5000);
-                Log.i(TAG, "CMDLINE : ==============================================================");
+                // only get information about the foreground app when it is with phone unlocked
+                if (!isScreenLocked()) {
+                    Log.d(TAG, "Phone is unlocked");
+                    String foregroundApp = getForegroundApp();
+                    // must save info in database
+                    handler.postDelayed(this, DELAY_SCREEN_UNLOCKED);
+                } else {
+                    Log.d(TAG, "Phone is locked");
+                    handler.postDelayed(this, DELAY_SCREEN_LOCKED);
+                }
+
             }
-        }, 2000);
+        }, DELAY_SCREEN_UNLOCKED);
 
         return super.onStartCommand(intent, flags, startId);
     }
 
-    public void getForegroundApp() {
-        PackageDao packageDao = new PackageDao(this);
-
-        String foregroundApp = ProcessManager.getForegroundApp();
-
-        packageDao.insert(foregroundApp);
-
-        Log.i(TAG, "LAST CMDLINE : " + foregroundApp);
-
-        packageDao.close();
-
+    /**
+     * Verify if the phone is locked or not
+     *
+     * @return boolean
+     */
+    private boolean isScreenLocked() {
+        KeyguardManager km = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        return km.inKeyguardRestrictedInputMode();
     }
 
 }
