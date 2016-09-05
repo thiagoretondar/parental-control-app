d989f73f55bdba943d8972e3ac016c5528cf6059
Thiago Retondar <t.retondar@gmail.com>
2016-09-05 20:04:09 -0300

    testing how to get location in background

--- app/src/main/java/fei/tcc/parentalcontrol/service/LocationInfoService.java
+++ app/src/main/java/fei/tcc/parentalcontrol/service/LocationInfoService.java
@@ -1,21 +1,25 @@
 package fei.tcc.parentalcontrol.service;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class LocationInfoService extends Service implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class LocationInfoService extends Service implements ConnectionCallbacks, OnConnectionFailedListener {
 
     private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
     private long FASTEST_INTERVAL = 2000; /* 2 sec */
@@ -27,6 +31,7 @@ public class LocationInfoService extends Service implements LocationListener, Go
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -37,8 +42,11 @@ public class LocationInfoService extends Service implements LocationListener, Go
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                mGoogleApiClient.connect();
-                handler.postDelayed(this, 5000);
+                if (checkPermission()) {
+                    mGoogleApiClient.connect();
+                   // mGoogleApiClient.disconnect();
+                }
+                handler.postDelayed(this, 2000);
             }
         }, 1000);
 
@@ -54,7 +62,6 @@ public class LocationInfoService extends Service implements LocationListener, Go
         } else {
             Log.i(TAG, "Location not found");
         }
-        startLocationUpdates();
     }
 
     @Override
@@ -82,24 +89,36 @@ public class LocationInfoService extends Service implements LocationListener, Go
         return null;
     }
 
-    @Override
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+/*    @Override
     public void onLocationChanged(Location location) {
         // New location has now been determined
         String msg = "Updated Location: " +
                 Double.toString(location.getLatitude()) + "," +
                 Double.toString(location.getLongitude());
         Log.i("LOCATION: ", msg);
-    }
+    }*/
 
     // Trigger new location updates at interval
-    protected void startLocationUpdates() {
+/*    protected void startLocationUpdates() {
         // Create the location request
         mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
                 .setInterval(UPDATE_INTERVAL)
                 .setFastestInterval(FASTEST_INTERVAL);
 
-        // Request location updates
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-    }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Request location updates
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }*/
+
 }
