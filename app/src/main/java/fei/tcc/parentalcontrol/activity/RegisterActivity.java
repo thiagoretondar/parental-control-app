6dba97350431cd92500615ea4737793d31125af0
Thiago Retondar <t.retondar@gmail.com>
2016-09-10 14:57:40 -0300

    creating login and register activities

---
+++ app/src/main/java/fei/tcc/parentalcontrol/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package fei.tcc.parentalcontrol.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fei.tcc.parentalcontrol.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
