c4816cacd379267684bcbb7f27b93af950de6b85
Thiago Retondar <t.retondar@gmail.com>
2016-10-31 08:28:14 -0200

    Configured API with Retrofit

--- app/src/main/java/fei/tcc/parentalcontrol/rest/APIPlug.java
+++ app/src/main/java/fei/tcc/parentalcontrol/rest/APIPlug.java
@@ -1,10 +1,15 @@
 package fei.tcc.parentalcontrol.rest;
 
+import fei.tcc.parentalcontrol.rest.dto.AllAppsInfoDto;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 /**
  * Created by thiagoretondar on 9/12/16.
  */
 public interface APIPlug {
 
-    // Endpoints of APIs
+    @POST("/app")
+    void sendAllAppsInfo(@Body AllAppsInfoDto allAppsInfoDto);
 
 }
